020963adc1a1ed90b5cc650a7f0765b4ae90a3fe
Víctor Manuel Herrera <vmherrera@vmherrera-macbook.local>
2024-01-11 13:27:19 +0100

    thebase version 3.2

--- src/components/IntroPost.jsx
+++ src/components/IntroPost.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom'
+// import { useHistory } from 'react-router-dom'
+// import { withRouter } from 'react-router-dom';
 
 import profilePic from '../images/profile-pic.png'
 // import GlobalData from '../assets/blogpost_data.json'
@@ -9,25 +11,33 @@ function IntroPost(PostData) {
   
   const IntroData = PostData.PostData
   const navigate=useNavigate()
+  // const history=useHistory()
+
+  // onClick={()=>navigate('../posts/'+IntroData.url_name)}
+  // onClick={() => history.push(`/posts/${IntroData.url_name}`)}
+
+  const postRoute = '../posts/' + IntroData.url_name;
 
   return (
-    <div id="intro_post_container" className='grid grid-cols-1
-    md:grid-cols-2 px-2.5 md:px-6
-    lg:px-0 gap-8 cursor-pointer' onClick={()=>navigate('../posts/'+IntroData.url_name)}>
-      <img src={ IntroData.cover } className='rounded-lg object-cover w-full h-[300px]'/>
-      <div className='px-6 md:px-0'>
-        <h4 className='text-red-500'>{ IntroData.attributes.tags }</h4>
-        <h4 className='text-[23px] font-bold mt-5'>{ IntroData.attributes.title }</h4>
-        <h4 className='line-clamp-6 text-gray-400 mt-5'>{ IntroData.attributes.description }</h4>
-        <div className='flex items-center mt-5'>
-          <img src={profilePic} className='w-[45px] rounded-full' />
-          <div className='ml-2'>
-            <h3 className='font-bold text-[16px]'>{ IntroData.author }</h3>
-            <h3 className='text-gray-500 text-[14px]'>{ IntroData.date }</h3>
+    <Link to={postRoute}>
+      <div id="intro_post_container" className='grid grid-cols-1
+      md:grid-cols-2 px-2.5 md:px-6
+      lg:px-0 gap-8 cursor-pointer' >
+        <img src={ IntroData.cover } className='rounded-lg object-cover w-full h-[300px]'/>
+        <div className='px-6 md:px-0'>
+          <h4 className='text-red-500'>{ IntroData.attributes.tags }</h4>
+          <h4 className='text-[23px] font-bold mt-5'>{ IntroData.attributes.title }</h4>
+          <h4 className='line-clamp-6 text-gray-400 mt-5'>{ IntroData.attributes.description }</h4>
+          <div className='flex items-center mt-5'>
+            <img src={profilePic} className='w-[45px] rounded-full' />
+            <div className='ml-2'>
+              <h3 className='font-bold text-[16px]'>{ IntroData.author }</h3>
+              <h3 className='text-gray-500 text-[14px]'>{ IntroData.date }</h3>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
